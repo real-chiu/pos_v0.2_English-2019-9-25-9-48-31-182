8b09130656afaaf5d7790c69d9199fd37dcab1b7
real-chiu <real.chiu@oocl.com>
2020-04-06 20:24:57 +0800

    getItemCount; addItemDetailsToItemCountList

--- main/main.js
+++ main/main.js
@@ -1,5 +1,37 @@
 'use strict';
 
 function printReceipt(inputs) {
-  console.log('Implement the exercise requirements here and rewrite the line of code.');
+
 }
+
+function getItemCount(itemList) {
+  let itemObject = {}
+  itemList.forEach(element => {
+    if (!itemObject[element]) 
+      itemObject[element] = 1;
+    else 
+      itemObject[element] += 1;
+  });
+
+  let itemCountList = []
+  for (let [key, value] of Object.entries(itemObject)) {
+    itemCountList.push({
+      itemId: key,
+      count: value
+    });
+  };
+  return itemCountList;
+}
+
+function addItemDetailsToItemCountList(itemCountList, allItems) {
+  return itemCountList.map(itemOfItemCountList => {
+    const matchedItem = allItems.find(item => item.barcode === itemOfItemCountList.itemId);
+    return {
+      ...itemOfItemCountList,
+      name: matchedItem.name,
+      unit: matchedItem.unit,
+      price: matchedItem.price
+    }
+  });
+}
+
